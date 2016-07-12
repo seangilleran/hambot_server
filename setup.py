9dd657d839c0f05a602645f1f5db5953b58b94dd
Sean Gilleran <sean@wgworkshop.com>
2016-07-11 17:15:33 -0700

    Add download_url to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='hambot-server',
-    version='0.1',
+    version='0.2',
     license='MIT',
     author='Sean Gilleran',
     author_email='sgilleran@gmail.com',
     url='https://github.com/seangilleran/hambot_server',
+    download_url='https://github.com/seangilleran/hambot_server/tarball/0.6',
     packages=['hambot_server'],
     install_requires=[
         'Flask>=0.11',
